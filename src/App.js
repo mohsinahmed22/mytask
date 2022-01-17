d8aa203be9c8e4b2031454e4d8ef52882b6026e6
mohsinahmed22 <ahmed.mohsin98@gmail.com>
2022-01-18 04:44:26 +0500

    App JS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./Assests/Css/responsive.css";
+
+import { DataProvider } from "./Context/Context";
+
+import Inputbar from "./Components/Inputbar/Inputbar";
+import TaskList from "./Components/List/List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <div className="container-wrapper">
+          <h1>My Task List</h1>
+          <div className="container">
+            <Inputbar />
+          </div>
+          <div className="taskList">
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </DataProvider>
   );
 }
 
